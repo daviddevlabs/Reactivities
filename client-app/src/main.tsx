25eb5298eef07f5864e8bb745d8eb2df3a4e5506
David Dev <daviddev204@gmail.com>
2024-08-29 13:28:43 +0300

    Implemented CRUD operations for the Activity entity in the frontend UI

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './app/Layout/App'
 import 'semantic-ui-css/semantic.min.css'
-import './index.css'
+import './app/Layout/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
