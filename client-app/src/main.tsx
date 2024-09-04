cf74451829bde3c2fcea727d200d0a08f19223be
David Dev <daviddev204@gmail.com>
2024-09-04 23:04:41 +0300

    Improved UI for better user experience

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
+import "react-calendar/dist/Calendar.css";
 import './app/Layout/styles.css'
 import { store, StoreContext } from './app/stores/store'
 import { router } from './app/router/route'
