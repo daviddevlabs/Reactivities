50f3a5fb1b7354cc95dd9edbf4150b74d42b9adf
David Dev <daviddev204@gmail.com>
2024-09-14 14:54:37 +0300

    Added exception handling in React component and backend

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 import "react-calendar/dist/Calendar.css";
+import "react-toastify/dist/ReactToastify.min.css";
 import './app/Layout/styles.css'
 import { store, StoreContext } from './app/stores/store'
-import { router } from './app/router/route'
+import { router } from './app/router/Routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
