339ce0733a3573e957c78e5063a54fbf5bf677dd
David Dev <daviddev204@gmail.com>
2024-09-26 11:01:37 +0300

    Added attendance feature on client side

--- client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import { Image, Item, Label, List, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
 
-export default function ActivityDetailedSidebar() {
+interface Props {
+    activity: Activity;
+}
+
+export default function ActivityDetailedSidebar({ activity: { attendees, host } }: Props) {
+
+    if (!attendees) return null;
+
     return (
         <>
             <Segment
@@ -12,26 +20,30 @@ export default function ActivityDetailedSidebar() {
                 inverted
                 color="teal"
             >
-                3 People Going
+                {attendees.length} {attendees.length === 1 ? "Person" : "People"} going
             </Segment>
             <Segment attached>
                 <List relaxed divided>
-                    <Item style={{ position: "relative" }}>
-                        <Label
-                            style={{ position: "absolute" }}
-                            color="orange"
-                            ribbon="right"
-                        >
-                            Host
-                        </Label>
-                        <Image size="tiny" src={"/assets/vite.svg"} />
-                        <Item.Content verticalAlign="middle">
-                            <Item.Header as="h3">
-                                <Link to={"#"}>Bob</Link>
-                            </Item.Header>
-                            <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
-                        </Item.Content>
-                    </Item>
+                    {attendees.map(attendee => (
+                        <Item style={{ position: "relative" }} key={attendee.username}>
+                            {attendee.username === host?.username && (
+                                <Label
+                                    style={{ position: "absolute" }}
+                                    color="orange"
+                                    ribbon="right"
+                                >
+                                    Host
+                                </Label>
+                            )}
+                            <Image size="tiny" src={attendee.image || "/assets/vite.svg"} />
+                            <Item.Content verticalAlign="middle">
+                                <Item.Header as="h3">
+                                    <Link to={`/profiles/${attendee.username}`}>{attendee.displayName}</Link>
+                                </Item.Header>
+                                <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+                            </Item.Content>
+                        </Item>
+                    ))}
                 </List>
             </Segment>
         </>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
 import LoadingComponent from "../../../app/Layout/LoadingComponent";
 import { Formik, Form } from "formik";
@@ -16,20 +16,11 @@ import DateInput from "../../../app/common/form/DateInput";
 
 export default observer(function ActivityForm() {
     const { activityStore } = useStore();
-    const { createActivity, updateActivity,
-        loading, loadActivity, loadingInitial } = activityStore;
+    const { createActivity, updateActivity, loadActivity, loadingInitial } = activityStore;
     const { id } = useParams();
     const navigate = useNavigate();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required("The activity title is required"),
@@ -41,10 +32,10 @@ export default observer(function ActivityForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!));
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)));
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity: Activity) {
+    function handleFormSubmit(activity: ActivityFormValues) {
         if (!activity.id) {
             activity.id = uuid();
             createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
@@ -80,7 +71,7 @@ export default observer(function ActivityForm() {
                         <TextInput placeholder="Venue" name="venue" />
                         <Button
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading={loading} floated="right"
+                            loading={isSubmitting} floated="right"
                             positive type="submit" content="Submit" />
                         <Button as={Link} to="/activities" floated="right" type="button" content="Cancel" />
                     </Form>
