55366e7b0f18121632b001c7e678f4c36403df45
David Dev <daviddev204@gmail.com>
2024-10-07 19:40:34 +0300

    Added comments feature with SignalR

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,7 +1,28 @@
 import { observer } from "mobx-react-lite"
-import { Button, Comment, Form, Header, Segment } from "semantic-ui-react"
+import { Comment, Header, Loader, Segment } from "semantic-ui-react"
+import { useStore } from "../../../app/stores/store";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Field, FieldProps, Form, Formik } from "formik";
+import * as Yup from "yup";
+import { formatDistanceToNow } from "date-fns";
 
-export default observer(function ActivityDetailedChat() {
+interface Props {
+    activityId: string;
+}
+
+export default observer(function ActivityDetailedChat({ activityId }: Props) {
+    const { commentStore } = useStore();
+
+    useEffect(() => {
+        if (activityId) {
+            commentStore.createHubConnection(activityId);
+        }
+        return () => {
+            commentStore.clearComments();
+        }
+    }, [commentStore, activityId]);
+
     return (
         <>
             <Segment
@@ -13,31 +34,50 @@ export default observer(function ActivityDetailedChat() {
             >
                 <Header>Chat about this event</Header>
             </Segment>
-            <Segment attached>
+            <Segment attached clearing>
+                <Formik
+                    onSubmit={(values, { resetForm }) => commentStore.addComment(values).then(() => resetForm())}
+                    initialValues={{ body: "" }}
+                    validationSchema={Yup.object({ body: Yup.string().required() })}
+                >
+                    {({ isSubmitting, isValid, handleSubmit }) => (
+                        <Form className="ui form">
+                            <Field name="body">
+                                {(props: FieldProps) => (
+                                    <div style={{ position: "relative" }}>
+                                        <Loader active={isSubmitting} />
+                                        <textarea
+                                            placeholder="Enter your comment"
+                                            rows={2}
+                                            {...props.field}
+                                            onKeyDown={e => {
+                                                if (e.key === "Enter" && e.shiftKey) {
+                                                    return;
+                                                }
+                                                if (e.key === "Enter" && !e.shiftKey) {
+                                                    e.preventDefault();
+                                                    isValid && handleSubmit();
+                                                }
+                                            }} />
+                                    </div>
+                                )}
+                            </Field>
+                        </Form>
+                    )}
+                </Formik>
                 <Comment.Group>
-                    <Comment>
-                        <Comment.Avatar src="/assets/vite.svg" />
-                        <Comment.Content>
-                            <Comment.Author as="a">Matt</Comment.Author>
-                            <Comment.Metadata>
-                                <div>Today at 5:42PM</div>
-                            </Comment.Metadata>
-                            <Comment.Text>How artistic!</Comment.Text>
-                            <Comment.Actions>
-                                <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                    </Comment>
-
-                    <Form reply>
-                        <Form.TextArea/>
-                        <Button
-                            content="Add Reply"
-                            labelPosition="left"
-                            icon="edit"
-                            primary
-                        />
-                    </Form>
+                    {commentStore.comments.map(comment => (
+                        <Comment key={comment.id}>
+                            <Comment.Avatar src={comment.image || "/assets/vite.svg"} />
+                            <Comment.Content>
+                                <Comment.Author as={Link} to={`/profiles/${comment.username}`}>{comment.displayName}</Comment.Author>
+                                <Comment.Metadata>
+                                    <div>{formatDistanceToNow(comment.createdAt)} ago</div>
+                                </Comment.Metadata>
+                                <Comment.Text style={{ whiteSpace: "pre-wrap" }}>{comment.body}</Comment.Text>
+                            </Comment.Content>
+                        </Comment>
+                    ))}
                 </Comment.Group>
             </Segment>
         </>
